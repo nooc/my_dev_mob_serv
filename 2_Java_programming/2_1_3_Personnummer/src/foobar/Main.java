8200f050a5857cafa823219a1b3abd1bd33eebe0
Ben Bright <nooc@users.noreply.github.com>
2022-09-26 16:23:02 +0200

    some polishing.

--- 2_Java_programming/2_1_3_Personnummer/src/foobar/Main.java
+++ 2_Java_programming/2_1_3_Personnummer/src/foobar/Main.java
@@ -19,7 +19,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         var pnPattern = Pattern.compile("^(?:19|20)?([0-9]{6})\\-?([0-9]{4})$");
         
-        int[] pn = new int[10]; // store pn as ints
+        int[] intPnArr = new int[10]; // store pn as ints
 
         // input loop
         while(true) {
@@ -27,17 +27,18 @@ public class Main {
             System.out.print("Swedish personal id number: ");
             String pnStr = input.nextLine().trim();
             // match
-            var mo = pnPattern.matcher(pnStr);
-            if(mo.matches()) {
-                var p1 = mo.group(1).toCharArray();
-                var p2 = mo.group(2).toCharArray();
-                // convert to int array
+            var matchObj = pnPattern.matcher(pnStr);
+            if(matchObj.matches()) {
+                var part1 = matchObj.group(1).toCharArray();
+                var part2 = matchObj.group(2).toCharArray();
+                // convert ascii to int array by subtracting ascii value for "0"
+                // from the char value, ie offset them to 0.
                 int pos = 0;
-                for (char c : p1) {
-                    pn[pos++] =  (int)c-48; // ascii for "0" is 48.
+                for (char c : part1) {
+                    intPnArr[pos++] =  (int)c-48; // ascii for "0" is 48.
                 }
-                for (char c : p2) {
-                    pn[pos++] =  (int)c-48;
+                for (char c : part2) {
+                    intPnArr[pos++] =  (int)c-48;
                 }
                 break;
             }
@@ -50,17 +51,17 @@ public class Main {
         // and then summing each digit of the product for each item.
         // The control is then 50-sum.
         // see: https://www.personnummer.nu/verifiera/ (Swedish)
-        int[] pn_val = {2,1,2,1,2,1,2,1,2}; // multiplication table
+        int[] mulTable = {2,1,2,1,2,1,2,1,2}; // multiplication table
         int sum = 0;
-        for (int i = 0; i < pn_val.length; ++i) {
-            int prod = pn[i] * pn_val[i];
-            sum += prod/10 + prod%10; // sum each digit, eg 12 would give 1+2=3 
+        for (int i = 0; i < mulTable.length; ++i) {
+            int product = intPnArr[i] * mulTable[i];
+            sum += product / 10 + product % 10; // sum each digit, eg 12 would give 1+2=3 
  
         }
         int control = 50 - sum;
         // valid if 10th pn number equals control.
-        boolean valid = control == pn[pn.length-1];
+        boolean valid = control == intPnArr[intPnArr.length-1];
 
-        System.out.println("Personnumret är " + (valid ? "korrekt." : "inkorrekt."));
+        System.out.println("Personal id number is " + (valid ? "correct." : "incorrect."));
     }
 }
